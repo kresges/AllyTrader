79025a7ee50226955ed7b6a8d1feb2e6ccfcc3ee
Spencer Kresge <spencerkresge@Spencers-MacBook.local>
2020-03-11 21:08:10 -0700

    Starting Files

---
+++ AllyTradeAPI.py
@@ -0,0 +1,22 @@
+import requests
+from requests_oauthlib import OAuth1
+import json
+
+from secrets import CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
+
+BASE_URL = 'https://api.tradeking.com/v1/'
+ACCOUNT_URL = '{}/accounts.json'.format(BASE_URL)
+
+def testAPI():
+    auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+    response = requests.get(ACCOUNT_URL, auth = auth).json()
+
+    print(response)
+
+def main():
+    print("AllyTradeAPI!")
+    testAPI()
+
+if __name__ == '__main__':
+    main()
