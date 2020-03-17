665b05999d0d279044c7fdd55f135c067ba25efe
kresges <kresges@oregonstate.edu>
2020-03-17 13:57:40 -0700

    Added several new ally_api end points.

--- AllyTradeAPI.py
+++ AllyTradeAPI.py
@@ -1,25 +1,12 @@
-import requests
-from requests_oauthlib import OAuth1
-import json
-
 from ally_api import AllyAPI
-from secrets import CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
-
-BASE_URL = 'https://api.tradeking.com/v1/'
-ACCOUNT_URL = '{}/accounts.json'.format(BASE_URL)
-
-def testAPI():
-    auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-    response = requests.get(ACCOUNT_URL, auth = auth).json()
-
-    print(response)
 
 def main():
     print("AllyTradeAPI!")
-    #testAPI()
+
     allyAPI = AllyAPI()
     print(allyAPI.get_user())
+    print(allyAPI.get_account_balance())
+    print(allyAPI.get_account_history())
 
 if __name__ == '__main__':
     main()
--- ally_api/api.py
+++ ally_api/api.py
@@ -1,10 +1,36 @@
 '''
 
 '''
+import json
+import requests
+from requests_oauthlib import OAuth1
+
+from secrets import CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
+
+BASE_URL = 'https://api.tradeking.com/v1/'
+ACCOUNT_URL = '{}/accounts.json'.format(BASE_URL)
+ACCOUNT_BALANCE_URL = '{}/accounts/balances.json'.format(BASE_URL)
 
 class AllyAPI():
     def __init__(self):
         self.user = "spencer"
+        self.auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        self._get_account_number()
+
+    def _get_account_number(self):
+        self.account_number = self.get_account_info()['accounts']['accountsummary']['account']
 
     def get_user(self):
         return self.user
+
+    def get_account_info(self):
+        return requests.get(ACCOUNT_URL, auth = self.auth).json()['response']
+
+    def get_account_balance(self):
+        return requests.get(ACCOUNT_BALANCE_URL, auth = self.auth).json()['response']
+
+    def get_account_history(self):
+        account_history_url = BASE_URL + '/accounts/{}/history.json'.format(self.account_number)
+        return requests.get(account_history_url, auth=self.auth).json()['response']
+
+    
